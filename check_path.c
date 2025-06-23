57fff4124339b69aca66934bda563bebe719f563
Maria Joao Fraga Balouta <mjoao-fr@c1r10s2.42porto.com>
2025-06-23 15:14:07 +0100

    still not working

--- check_path.c
+++ check_path.c
@@ -6,7 +6,7 @@
 /*   By: mjoao-fr <mjoao-fr@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 12:06:25 by mjoao-fr          #+#    #+#             */
-/*   Updated: 2025/06/23 14:41:26 by mjoao-fr         ###   ########.fr       */
+/*   Updated: 2025/06/23 15:11:41 by mjoao-fr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ void	fill(char **dup, t_point current, t_map *map, char to_avoid1)
 	if ((current.x < 0 || current.y < 0 || current.x >= map->width
 			|| current.y >= map->height)
 		|| dup[current.y][current.x] == to_avoid1
-	|| dup[current.y][current.x] == 'E')
+	|| dup[current.y][current.x] == 'E'
+	|| dup[current.y][current.x] == 'R')
 		return ;
-	dup[current.y][current.x] = '1';
+	dup[current.y][current.x] = 'R';
 	fill(dup, (t_point){current.x, current.y - 1}, map, '1');
 	fill(dup, (t_point){current.x, current.y + 1}, map, '1');
 	fill(dup, (t_point){current.x - 1, current.y}, map, '1');
@@ -52,6 +53,12 @@ char	**flood_fill(t_map *map, t_player *player)
 			i++;
 		}
 	}
+	j = 0;
+	while (dup[j])
+	{
+		ft_printf("%s\n", dup[j]);
+		j++;
+	}
 	return (dup);
 }
 
@@ -60,21 +67,29 @@ int	validate_path(t_map *map, t_player *player)
 	char	**dup;
 	int		i;
 	int		j;
+	int		count_c;
+	int		count_e;
 
 	dup = flood_fill(map, player);
 	j = 0;
 	i = 0;
+	count_c = 0;
+	count_e = 0;
 	while (dup[j])
 	{
 		i = 0;
 		while (dup[j][i])
 		{
 			if (dup[j][i] == 'C')
-				return (free_map(dup, map->height), 1);
+				count_c++;
+			if (dup[j][i] == 'E')
+				count_e++;
 			i++;
 		}
 		j++;
 	}
+	if (!count_e && !count_c)
+		return (free_map(dup, map->height), 1);
 	free_map(dup, map->height);
 	return (0);
 }
